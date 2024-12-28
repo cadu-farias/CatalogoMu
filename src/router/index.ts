6eef15c202b1e13f13c540c91f6a20fe458409ca
Carlos Eduardo <cadu.giul@gmail.com>
2024-12-28 14:48:03 -0300

    adicionado tratamento 404 para rotas não existente

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router/auto';
 import { routes } from 'vue-router/auto-routes';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
+// Adiciona rota coringa manualmente
+routes.push({
+  path: '/:pathMatch(.*)*', // Captura todas as rotas não reconhecidas
+  component: () => import('@/pages/error-404/index.vue'), // Página 404 personalizada
+})
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
